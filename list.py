42dd4b8f4b88ff5fc2d03d8f69b8529049aa73fa
OwenHipfner <89998883+OwenHipfner@users.noreply.github.com>
2022-01-19 17:37:55 +0000

    added historie file

--- list.py
+++ list.py
@@ -4,7 +4,16 @@
 
 #Total list of websites that the web browser has. This can be imported to the main file.
 list_website = '''
-Wikipedia, Google, Youtube, Twitch, Amazon, Netflix, Disney, Best Buy, Bing, Yahoo,
+Wikipedia, Google, Youtube, Twitch, Amazon, Netflix, Disney, Best Buy, Bing, Yahoo, 
+Microsoft, Hp, Apple, Duolingo, Samsung, Vans, Nike, Addidas, Dell, Sasktell, Access, 
+Replit, Github, Spell Check Plus, Bonpatron, Linguee, Google Classroom, Google Slides, 
+Google Docs, Google Drive, Gmail, Google Maps, Desmos, Google Translate, Lego, Kahoot, 
+Reverso Context, Grammarly, Edsby, Remind, MyBluePrint, Oculus, Instagram, Snapchat, Whatsapp, 
+Xbox, Playstation, Spotify, Discord, Tiktok, SkiptheDishes, DoorDash, Uber Eats, Uber, Macdonalds, 
+Burger King, Weather(Network), Facebook, Reddit, Cbc, Global, Ctv, Radio Canada, Soundcloud, Intel, 
+Nvidia, Amd, HyperX, Corsair, Newegg, Prezi, Amazon Prime, Pintrest, Sephora, American Eagle, Urban Outfitters, 
+Kijiji, Starbucks, Logitech, Sport Chek, Canadian Tire, Marks, Razer, Astro, Home Depot, A&W, Urban Dictionairy, 
+Costco, Lululemon, London Drugs, OtterBox, Old Navy, Polaris, Ski-doo, Sea-doo, Can-Am, Yamaha, Arctic Cat, 509        
 '''
 #The title of my web browserthat can be imported to my main file.
 title_website = '''
